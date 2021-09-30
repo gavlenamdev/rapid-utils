74da733d552ab29b455472d85df26d39374bb554
Namdev Gavle <gavlenamdevprabha@gmail.com>
2021-09-30 17:58:32 +0530

    requirements updated

--- rapid_utils/__init__.py
+++ rapid_utils/__init__.py
@@ -2,4 +2,4 @@ from .core import download_file, is_allowed_extension, is_json, get_file_size
 
 __all__ = ['download_file', 'is_allowed_extension', 'is_json', 'get_file_size']
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="rapid-utils",
-    version="0.0.4",
+    version="0.0.5",
     description="high level functions",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/gnaam/rapid-utils",
-    download_url="https://github.com/gnaam/rapid-utils/archive/v0.0.4.tar.gz",
+    download_url="https://github.com/gnaam/rapid-utils/archive/v0.0.5.tar.gz",
     author="Namdev Gavle",
     author_email="gavlenamdevprabha@gmail.com",
     license="MIT",
     packages=find_packages(),
-    install_requires=["requests==2.22.0"],
+    install_requires=["requests>=2.22.0"],
     include_package_data=True,
     classifiers=[
         "License :: OSI Approved :: MIT License",
